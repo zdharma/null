a9a35e6a5f08163dd09703e1a124c53527c4561d
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-09-22 03:55:11 +0200

    null.plugin.zsh

---
+++ null.plugin.zsh
@@ -0,0 +1,7 @@
+# Handle $0 according to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# An empty file – only a single command gets executed
+true
